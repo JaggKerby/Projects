719ebd080ec51c1990caaed79944c34c7e2e6ee6
JaggKerby <ilyakerby@gmail.com>
2020-06-03 21:53:38 +0300

    in progress

--- Test_maket/app/js/main.js
+++ Test_maket/app/js/main.js
@@ -25,124 +25,97 @@ const config = {
 // Вешаем наблюдатель на все елементы details
 document.querySelectorAll('div details').forEach(el => observer.observe(el, config))
 
-// Слайдер скриншотов программ. 
 
-$(function () {
-  $('.slider').slick({
-    nextArrow: '<button class="slick-arrow slick-prev"><img src="img/prev.svg" alt="prev arrow"></button>',
-    prevArrow: '<button class="slick-arrow slick-next"><img src="img/next.svg" alt="next arrow"></button>',
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    responsive: [{
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '400px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
-});
-
-// new Vue({
-//     el: '#example',
-//     data: {
-//       slides: 7
-//     },
-//     components: {
-//       'carousel-3d': Carousel3d.Carousel3d,
-//       'slide': Carousel3d.Slide
-//     }
-//   })
-
-
-
-//запуск видео 
-
-// let videoElem = document.getElementById("videoPlayer");
-// let playButton = document.getElementById("playbutton");
-
-// playButton.addEventListener("click", handlePlayButton, false);
-// playVideo();
-
-// async function playVideo() {
-//   try {
-//     await videoElem.play();
-//     playButton.classList.add("playing");
-//   } catch(err) {
-//     playButton.classList.remove("playing");
-//   }
-// }
-
-// function handlePlayButton() {
-//   if (videoElem.paused) {
-//     playVideo();
-//   } else {
-//     videoElem.pause(); 
-//     playButton.classList.remove("playing");
-//   }
-// }
-
-// var button = document.getElementById('playbutton');
-// var vid = document.getElementById('videoPlayer');
-
-// if(button.addEventListener){
-// 		button.addEventListener("click", play, false)
-// 	}else if(button.attachEvent){
-// 		button.attachEvent("onclick", play)
-// 	}
-
-// function play() { 
-//     if (vid.paused){
-//         vid.play(); 
-//         button =  ;
-//     }else {
-//         vid.pause(); 
-//         // button.className = "";
-//     }
-// } 
+//feedback slider
 $(function () {
   $('.feedback__slider').slick({
     prevArrow: '<button class="feedback__btn arrow-prev"><i class="fas fa-long-arrow-alt-left"></i></button>',
     nextArrow: '<button class="feedback__btn arrow-next"><i class="fas fa-long-arrow-alt-right"></i></button>',
     infinite: true,
-    // slidesToShow: 3,
-    // slidesToScroll: 1
     centerMode: true,
-    centerPadding: '0px',
     slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 1500,
         settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 2
+          slidesToShow: 2,
+          slidesToScroll: 1,
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 900,
         settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
+          slidesToShow: 1,
+          slidesToScroll: 1,
         }
       }
+
     ]
   });
 });
 
+//Screenshots slider
+
+$(document).ready(function () {
+  $("#carousel").waterwheelCarousel({
+    // number tweeks to change apperance
+    startingItem: 1, // item to place in the center of the carousel. Set to 0 for auto
+    separation: 205, // distance between items in carousel
+    separationMultiplier: 0.4, // multipled by separation distance to increase/decrease distance for each additional item
+    horizonOffset: 0, // offset each item from the "horizon" by this amount (causes arching)
+    horizonOffsetMultiplier: 0.5, // multipled by horizon offset to increase/decrease offset for each additional item
+    sizeMultiplier: 0.7, // determines how drastically the size of each item changes
+    opacityMultiplier: 1, // determines how drastically the opacity of each item changes
+    horizon: 0, // how "far in" the horizontal/vertical horizon should be set from the container wall. 0 for auto
+    flankingItems: 3, // the number of items visible on either side of the center                  
+
+    // animation
+    speed: 200, // speed in milliseconds it will take to rotate from one to the next
+    animationEasing: 'linear', // the easing effect to use when animating
+    quickerForFurther: true, // set to true to make animations faster when clicking an item that is far away from the center
+    edgeFadeEnabled: false, // when true, items fade off into nothingness when reaching the edge. false to have them move behind the center image
+
+    // misc
+    linkHandling: 2, // 1 to disable all (used for facebox), 2 to disable all but center (to link images out)
+    autoPlay: 0, // indicate the speed in milliseconds to wait before autorotating. 0 to turn off. Can be negative
+    orientation: 'horizontal', // indicate if the carousel should be 'horizontal' or 'vertical'
+    activeClassName: 'carousel-center', // the name of the class given to the current item in the center
+    keyboardNav: false, // set to true to move the carousel with the arrow keys
+    keyboardNavOverride: true, // set to true to override the normal functionality of the arrow keys (prevents scrolling)
+    imageNav: true, // clicking a non-center image will rotate that image to the center
+
+    // preloader
+    preloadImages: true, // disable/enable the image preloader. 
+    forcedImageWidth: 0, // specify width of all images; otherwise the carousel tries to calculate it
+    forcedImageHeight: 0, // specify height of all images; otherwise the carousel tries to calculate it
+
+    // callback functions
+    movingToCenter: $.noop, // fired when an item is about to move to the center position
+    movedToCenter: $.noop, // fired when an item has finished moving to the center
+    clickedCenter: $.noop, // fired when the center item has been clicked
+    movingFromCenter: $.noop, // fired when an item is about to leave the center position
+    movedFromCenter: $.noop, // fired when an item has finished moving from the center
+
+  });
+
+  $('#prev').bind('click', function () {
+    carousel.prev();
+    return false
+  });
+
+  $('#next').bind('click', function () {
+    carousel.next();
+    return false;
+  });
+
+});
+
+// video player 
+
+
+$(document).on('click', '.videowall__btn', function () {
+  var $video = $('#videoPlayer'),
+    src = $video.attr('src');
+
+  $video.attr('src', src + '&autoplay=1');
+});
